1efc9a0cb308d82b904e4f30779a33f8acf04f61
Michlas Cliff Lund-Mouritzen <Deva_i_tiden@hotmail.com>
2019-07-26 01:08:09 +0200

    Update Source.cpp
    added some comments and seperated the 3 cellular automata codes....

--- Source.cpp
+++ Source.cpp
@@ -25,6 +25,8 @@ public:
 		sAppName = "This is the main window";
 	}
 
+	/////////////////////////////////////// COUNTS WHAT TILE SHOULD BE DRAWN //////////////////////
+
 	int count(int x, int y) {
 		int i = 0;
 		if (bMap[wrap(x)][wrap(y)]) i += 1;
@@ -40,18 +42,7 @@ public:
 		return i;
 	}
 
-	bool golCount(int x, int y) {
-		int r= 0;
-		for (int i = -1; i < 2; i++) for (int j = -1; j < 2; j++) if (bMap[wrap(x + i)][wrap(y + j)] && (i != 0 || j != 0)) r++;
-		if (bMap[x][y]) {
-			if (r == 2 || r == 3) return true;
-			return false;
-		}
-		else if (r == 3) return true;
-		return false;
-	}
-
-
+	///////////////////////////////// UPDATE THE TILEMAP INDEXES ////////////////////////////////
 	void setIntMap() {
 		for (int i = 0; i < mapSize; i++) {
 			for (int j = 0; j < mapSize; j++) {
@@ -60,6 +51,8 @@ public:
 			}
 		}
 	}
+
+	//////////////////////////////// MAKE RANDOM MAP AND TURN OFF ANY CHANGES ///////////////////////////////////
 	void randMap() {
 		int val = rand() % 9+ 2;
 		for (int i = 0; i < mapSize; i++) {
@@ -71,72 +64,99 @@ public:
 		}
 		grav = false;
 	}
+
+
+	////////////////////////////////////// FALKE GRAVITY PHYSICS ///////////////////////////////////////////
+	void fallAlgorithm() {
+		for (int j = 0; j < mapSize - 1; j++) {
+			for (int i = 0; i < mapSize; i++) {
+				if (bMap[i][j] && !xMap[i][j]) {
+					int r = rand() % 3 - 1;
+					if (!bMap[wrap(i)][j + 1]) {
+						bMap[i][j] = false;
+						bMap[wrap(i)][j + 1] = true;
+						xMap[wrap(i)][j + 1] = true;
+					}
+					else if (!bMap[wrap(i + r)][j + 1] && rand() % 7 == 0) {
+						bMap[i][j] = false;
+						bMap[wrap(i + r)][j + 1] = true;
+						xMap[wrap(i + r)][j + 1] = true;
+					}
+					else if (!bMap[wrap(i + r * 2)][j + 1] && !bMap[wrap(i + r)][j] && rand() % 20 == 0) {
+						bMap[i][j] = false;
+						bMap[wrap(i + r)][j] = true;
+						xMap[wrap(i + r)][j] = true;
+					}
+					else if (rand() % 10000000 < j / 20) {
+						bool done = false;
+						int count = 0;
+						while (!done && count < mapSize) {
+							int x = rand() % mapSize;
+							count++;
+							if (!bMap[x][0]) {
+								bMap[i][j] = false;
+								bMap[x][0] = true;
+								done = true;
+								xMap[x][0] = true;
+							}
+						}
+					}
+				}
+			}
+
+		}
+	}
+
+	/////////////////////////////////////////////////////// JUST SEMI RANDOM GROWTH //////////////////////////////////////////
+	void growthAlgorithm() {
+		{
+			for (int k = 0; k < mapSize; k++) {
+				int i = rand() % mapSize;
+				int j = rand() % mapSize;
+				if (bMap[i][j]) {
+					int r1 = rand() % 3 - 1;
+					int r2 = rand() % 3 - 1;
+					if (r1 != 0 || r2 != 0 || rand() % 5 == 0) bMap[wrap(i + r1)][wrap(j + r2)] = !bMap[wrap(i + r1)][wrap(j + r2)];
+				}
+			}
+		}
+	}
+
+	//////////////////////////////////////////////// GAME OF LIFE /////////////////////////////////////////////////////////////////
+	bool golCount(int x, int y) {
+		int r = 0;
+		for (int i = -1; i < 2; i++) for (int j = -1; j < 2; j++) if (bMap[wrap(x + i)][wrap(y + j)] && (i != 0 || j != 0)) r++;
+		if (bMap[x][y]) {
+			if (r == 2 || r == 3) return true;
+			return false;
+		}
+		else if (r == 3) return true;
+		return false;
+	}
+
+	void gameOfLife() {
+		{
+			bool tMap[mapSize][mapSize];
+			for (int j = 0; j < mapSize; j++)
+				for (int i = 0; i < mapSize; i++)
+					tMap[i][j] = golCount(i, j);
+
+			for (int j = 0; j < mapSize; j++)
+				for (int i = 0; i < mapSize; i++)
+					bMap[i][j] = tMap[i][j];
+		}
+	}
+
+	///////////////////////////////////////////////// UPDATE MAP /////////////////////////////////////////////////////////////////////
 	void updateMap(float fElapsedTime) {
 		static float timer = 0;
 		timer += fElapsedTime;
 		if (timer > 1.0f / 50 || !grav) {
 			timer = 0;
-			if (grav && mapSelect == 2) {
-				for (int j = 0; j < mapSize - 1; j++) {
-					for (int i = 0; i < mapSize; i++) {
-						if (bMap[i][j] && !xMap[i][j]) {
-							int r = rand() % 3 - 1;
-							if (!bMap[wrap(i)][j + 1]) {
-								bMap[i][j] = false;
-								bMap[wrap(i)][j + 1] = true;
-								xMap[wrap(i)][j + 1] = true;
-							}
-							else if (!bMap[wrap(i + r)][j + 1] && rand() % 7 == 0) {
-								bMap[i][j] = false;
-								bMap[wrap(i + r)][j + 1] = true;
-								xMap[wrap(i + r)][j + 1] = true;
-							}
-							else if (!bMap[wrap(i + r * 2)][j + 1] && !bMap[wrap(i + r)][j] && rand()% 20 == 0) {
-								bMap[i][j] = false;
-								bMap[wrap(i + r)][j] = true;
-								xMap[wrap(i + r)][j] = true;
-							}
-							else if (rand() % 10000000 < j / 20) {
-								bool done = false;
-								int count = 0;
-								while (!done && count < mapSize) {
-									int x = rand() % mapSize;
-									count++;
-									if (!bMap[x][0]) {
-										bMap[i][j] = false;
-										bMap[x][0] = true;
-										done = true;
-										xMap[x][0] = true;
-									}
-								}
-							}
-						}
-					}
+			if (grav && mapSelect == 2) fallAlgorithm();
+			else if (grav && mapSelect == 1) growthAlgorithm();
 
-				}
-			}
-			else if (grav && mapSelect == 1) {
-				for (int k = 0; k < mapSize; k++) {
-					int i = rand() % mapSize;
-					int j = rand() % mapSize;
-					if (bMap[i][j]) {
-						int r1 = rand() % 3 - 1;
-						int r2 = rand() % 3 - 1;
-						if (r1 != 0 || r2 != 0 || rand() % 5 == 0) bMap[wrap(i + r1)][wrap(j + r2)] = !bMap[wrap(i + r1)][wrap(j + r2)];
-					}
-				}
-			}
-
-			else if (grav && mapSelect == 0) {
-				bool tMap[mapSize][mapSize];
-				for (int j = 0; j < mapSize; j++)
-					for (int i = 0; i < mapSize; i++)
-						tMap[i][j] = golCount(i, j);
-
-				for (int j = 0; j < mapSize; j++)
-					for (int i = 0; i < mapSize; i++)
-						bMap[i][j] = tMap[i][j];
-			}
+			else if (grav && mapSelect == 0) gameOfLife();
 			setIntMap();
 		}
 	}
